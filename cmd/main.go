75c4355fac78afbc278d134a3502f786d468ea5d
john <john@x-tek.ru>
2020-08-08 16:20:44 +0200

    v0.02-jwt-token

--- cmd/main.go
+++ cmd/main.go
@@ -1,80 +1,90 @@
 package main
 
 import (
-​    "net/http"
+	"os"
 
-    "github.com/gorilla/mux"
-​    "github.com/gorilla/handlers" // Пакет с набором готовых хендлеров
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	"github.com/dgrijalva/jwt-go"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
+// AppKey ...
+const AppKey = "secret"
 
+// main func...
 func main() {
 
-​    rt := mux.NewRouter()
+	router := mux.NewRouter()
 
-    rt.Handle("/status", StatusHandler).Methods("GET")
-    rt.Handle("/get-token", GetTokenHandler).Methods("GET")
-    rt.Handle("/test", jwtMiddleware.Handler(TestHandler)).Methods("GET")
-    
-    http.ListenAndServe( ":3000", handlers.LoggingHandler(os.Stdout, r) )
+	router.HandleFunc("/status", StatusHandler).Methods("GET")
+	router.HandleFunc("/token", TokenHandler).Methods("GET")
+	router.Handle("/", AuthMiddleware(http.HandlerFunc(ExampleHandler)))
 
-//--------------------------------------------------------------------------------
+	http.ListenAndServe(":8001", handlers.LoggingHandler(os.Stdout, router))
 
-	type TempSt struct {
-	​    Id    int
-	​    Name  string
-	​    Slug  string
-	}
+}
 
-	var temtdata = []TempSt{
-	​    TempSt{Id: 1, Name: "Hover Shooters", Slug: "hover-shooters"},
-	​    TempSt{Id: 2, Name: "Ocean Explorer", Slug: "ocean-explorer"},
-	}
+// StatusHandler ...
+func StatusHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("my Not Implemented /status"))
+}
 
+// ExampleHandler ...
+func ExampleHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	io.WriteString(w, `{"status":"ok"}`)
+}
 
-	var TestHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-	​    // Конвертируем в json
-	​    payload, _ := json.Marshal(tempdata)
-	    w.Header().Set("Content-Type", "application/json")
-	    w.Write([]byte(payload))
-		})
+// TokenHandler ...
+func TokenHandler(w http.ResponseWriter, r *http.Request) {
 
-//---------------------------------------------------------------------------------
+	w.Header().Add("Content-Type", "application-json")
+	// r.ParseForm()
+	username := "Tom Cruse" // r.Form.Get("username")
+	//password := "qwerty"    // r.Form.Get("password")
+	// if username != "myusername" || password != "mypassword" {
+	// 	w.WriteHeader(http.StatusUnauthorized)
+	// 	io.WriteString(w, `{"error":"invalid_credentials"}`)
+	// 	return
+	// }
 
-	var StatusHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-	​    w.Write([]byte("Hello world API is up and running."))
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"name": username,
+		"role": "admin",
+		"exp":  time.Now().Add(time.Hour * time.Duration(1)).Unix(),
 	})
 
-//---------------------------------------------------------------------------------
+	tokenString, err := token.SignedString([]byte(AppKey))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		io.WriteString(w, `{"error!":"token_generation_faled"}`)
+		return
+	}
 
-    // Глобальный секретный ключ
-    var mySigningKey = []byte("secret")
+	//io.WriteString(w, `{"token":"`+tokenString+`"}`)
+	w.Write([]byte(tokenString))
+	return
+}
 
-    var GetTokenHandler = http.HandlerFunc(func(w http.ResponseWriter, 
-                                            r *http.Request){
-        // Создаем новый токен
-        token := jwt.New(jwt.SigningMethodHS256)
-    
-        // Устанавливаем набор параметров для токена
-        token.Claims["admin"] = true
-        token.Claims["name"] = "First Last"
-        token.Claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
-    
-        // Подписываем токен нашим секретным ключем
-        tokenString, _ := token.SignedString(mySigningKey)
-    
-        // Отдаем токен клиенту
-        w.Write([]byte(tokenString))
-    })
+// AuthMiddleware ...
+func AuthMiddleware(next http.Handler) http.Handler {
+	if len(AppKey) == 0 {
+		log.Fatal("HTTP server unable to start, expected an AppKey for JWT auth")
+	}
 
-//-------------------------------------------------------------------------------------
-	
-	// проверка токена
-	var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
-	    ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
-	        return mySigningKey, nil
-	    },
-	    SigningMethod: jwt.SigningMethodHS256,
+	jwtmiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return []byte(AppKey), nil
+		},
+		SigningMethod: jwt.SigningMethodHS256,
 	})
 
+	return jwtmiddleware.Handler(next)
 }
